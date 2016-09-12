b09aad2ab2304cbeeb089d4291e6b89a83f2f2a8
Francisco Puyó <Francisco Puy>
2016-09-11 21:30:13 -0300

    Inicio el proyecto y la coneccion a la DB

---
+++ ClinicaFrba/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClinicaFrba.util;
+
+namespace ClinicaFrba
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            Sql.connect("gd");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ ClinicaFrba/util/Sql.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace ClinicaFrba.util
+{
+    class Sql
+    {
+        public static SqlConnection connect(string connectionString)
+        {
+            SqlConnection connection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings[connectionString].ConnectionString);
+            try
+            {
+                connection.Open();
+                MessageBox.Show("Me conecte a la base");
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Fail connecting!" + ex.Message);
+            }
+
+            return connection;
+        }
+    }
+}
