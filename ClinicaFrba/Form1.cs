66d5e0433de785374af46e1a4ece95f7a01beb16
martinvergara26 <martin.vergara@tictapps.com>
2016-10-27 22:03:11 -0300

    Control de cantidad de intentos de login

--- ClinicaFrba/Form1.cs
+++ ClinicaFrba/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using ClinicaFrba.util;
 using System.Security.Cryptography;
+using ClinicaFrba.Seleccion_Rol;
 
 namespace ClinicaFrba
 {
@@ -24,20 +25,60 @@ namespace ClinicaFrba
             String user = textBox1.Text;
             String pass = textBox2.Text;
 
-            String query = "select * from Usuarios where usuario = {0} and contrasena = HASHBYTES('SHA2_256', '{1}')";
-            query = String.Format(query, user, pass);
+            String userExists = "select * from Usuarios where usuario = {0}";
+            userExists = String.Format(userExists, user);
 
-            DataTable results = Sql.query(query);
+            DataTable userExistsResults = Sql.query(userExists);
 
-            int count = results.Rows.Count;
-
-            if (count == 0)
+            if (userExistsResults.Rows.Count == 0)
             {
-                MessageBox.Show("Todo mal");
+                MessageBox.Show("Usuario inexistente");
                 return;
             }
 
-            MessageBox.Show("Bien ahi");
+            Boolean activo = (Boolean) userExistsResults.Rows[0][2];
+            if (!activo)
+            {
+                MessageBox.Show("Usuario deshabilitado");
+                return;
+            }
+
+            String userPassQuery = "select * from Usuarios where usuario = {0} and contrasena = HASHBYTES('SHA2_256', '{1}')";
+            userPassQuery = String.Format(userPassQuery, user, pass);
+
+            DataTable userResults = Sql.query(userPassQuery);
+
+            int count = userResults.Rows.Count;
+
+            if (count == 0) {
+                MessageBox.Show("Contrasena inválida");
+                String incrementWrongTries = "update usuarios set cantidad_intentos_login = cantidad_intentos_login + 1 where usuario = {0}";
+                incrementWrongTries = String.Format(incrementWrongTries, user);
+
+                Sql.update(incrementWrongTries);
+
+                int tries = (int) userExistsResults.Rows[0][3];
+                if (tries + 1 == 3)
+                {
+                    String unactivate = "update usuarios set activo = 0 where usuario = {0}";
+                    unactivate = String.Format(unactivate, user);
+                    Sql.update(unactivate);
+                    MessageBox.Show("Fuiste deshabilitado por intentar muchas veces");
+                }
+                return;
+            } 
+            
+            
+            MessageBox.Show("Todo bien");
+            Session.user = user;
+            String cleanWrongTries = "update usuarios set cantidad_intentos_login = 0 where usuario = {0}";
+            cleanWrongTries = String.Format(cleanWrongTries, user);
+            Sql.update(cleanWrongTries);
+
+            this.Hide();
+            RoleSelect roleSelect = new RoleSelect();
+            roleSelect.Show();
+            
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
