b0c7e07656b1394818ac0b2acb646b005d19f7bd
Francisco Puyó <Francisco Puy>
2016-11-04 17:57:06 -0300

    mend

---
+++ ClinicaFrba/Agenda Medico/ElegirProfesional.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ClinicaFrba.Agenda_Medico
+{
+    partial class ElegirProfesional
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.especialidadesCombo = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buscar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // especialidadesCombo
+            // 
+            this.especialidadesCombo.FormattingEnabled = true;
+            this.especialidadesCombo.Location = new System.Drawing.Point(63, 43);
+            this.especialidadesCombo.Name = "especialidadesCombo";
+            this.especialidadesCombo.Size = new System.Drawing.Size(196, 21);
+            this.especialidadesCombo.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(60, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Seleccione el profesional con el que sera atendido";
+            // 
+            // buscar
+            // 
+            this.buscar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.buscar.Location = new System.Drawing.Point(497, 41);
+            this.buscar.Name = "buscar";
+            this.buscar.Size = new System.Drawing.Size(75, 23);
+            this.buscar.TabIndex = 7;
+            this.buscar.Text = "Buscar";
+            this.buscar.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(63, 85);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(509, 242);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // ElegirProfesional
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 345);
+            this.Controls.Add(this.especialidadesCombo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buscar);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "ElegirProfesional";
+            this.Text = "Elegir profesional";
+            this.Load += new System.EventHandler(this.ElegirProfesional_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox especialidadesCombo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buscar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ ClinicaFrba/Agenda Medico/ElegirProfesional.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using ClinicaFrba.Abm_Especialidades_Medicas;
+using ClinicaFrba.util;
+
+namespace ClinicaFrba.Agenda_Medico
+{
+    public partial class ElegirProfesional : Form
+    {
+
+        private BindingSource bindingSource;
+        private SqlDataAdapter adapter;
+
+        public ElegirProfesional()
+        {
+            InitializeComponent();
+        }
+
+        private void ElegirProfesional_Load(object sender, EventArgs e)
+        {
+            loadEspecialidades();
+            loadProfesionales();
+        }
+
+        private void loadProfesionales()
+        {
+            SqlConnection connection = util.Sql.connect("gd");
+
+            String query = "select nombre as Nombre, apellido as Apellido, Especialidades.descripcion, Profesionales.profesional_dni as Documento,'Seleccionar' as Seleccionar from  Profesionales, Especialidades, Medico_Especialidad, Personas_Detalle where  Medico_Especialidad.profesional_dni = Profesionales.profesional_dni and Medico_Especialidad.especialidad_codigo = Especialidades.codigo and Personas_Detalle.dni = Profesionales.profesional_dni and Especialidades.descripcion = '{0}'";
+            query = String.Format(query, especialidadesCombo.Text);
+            adapter = new SqlDataAdapter(query, connection);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+
+            bindingSource = new BindingSource();
+            bindingSource.DataSource = dataTable;
+
+            dataGridView1.DataSource = bindingSource;
+
+            adapter.Update(dataTable);
+        }
+
+        private void loadEspecialidades()
+        {
+            DataTable especialidades = util.Sql.query("select descripcion from Especialidades");
+            List<String> options = new List<string>();
+            for (int i = 0; i < especialidades.Rows.Count; i++)
+            {
+                options.Add(especialidades.Rows[i][0].ToString());
+            }
+            especialidadesCombo.DataSource = options;
+        }
+
+        private void buscar_Click(object sender, EventArgs e)
+        {
+            loadProfesionales();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            String accion = dataGridView1.Columns[e.ColumnIndex].HeaderText.ToString();
+            String dni = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+            String nombre = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            String apellido = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            this.Hide();
+
+            Agendas sel = new Agendas(Int32.Parse(dni));
+            sel.Show();
+        }
+
+        private void volver_Click(object sender, EventArgs e)
+        {
+            Session.mainMenu(this);
+        }
+
+    }
+}
