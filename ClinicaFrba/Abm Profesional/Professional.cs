1c3f55811adb81a800c8b75021a2b8fe6ecb82ee
Francisco Puyó <Francisco Puy>
2016-11-04 16:50:50 -0300

    agrego clase professional

---
+++ ClinicaFrba/Abm Profesional/Professional.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Threading.Tasks;
+using ClinicaFrba.util;
+namespace ClinicaFrba.Abm_Profesional
+{
+    class Professional
+    {
+        public static DataTable getProfessionalByDni(int dni)
+        {
+            String query = "SELECT * from Profesionales p JOIN Personas_Detalle pd ON (p.profesional_dni = pd.dni) WHERE profesional_dni = {0}";
+            query = String.Format(query, dni.ToString());
+            return Sql.query(query);
+        }
+    }
+}
