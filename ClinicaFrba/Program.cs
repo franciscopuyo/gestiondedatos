3f65ddde18e7188d176619e7681b338810effebd
Francisco Puyó <Francisco Puy>
2016-11-20 20:37:04 -0300

    creo getdatecustom

--- ClinicaFrba/Program.cs
+++ ClinicaFrba/Program.cs
@@ -15,6 +15,15 @@ namespace ClinicaFrba
         [STAThread]
         static void Main()
         {
+           
+            String query = "IF EXISTS(" +
+            "SELECT * FROM sysobjects WHERE id = object_id(N'group_by.GETDATECUSTOM')" +
+            "AND xtype IN(N'FN', N'IF', N'TF')" +
+             ")  DROP FUNCTION group_by.GETDATECUSTOM";
+            Sql.update(query);
+            query = "CREATE FUNCTION group_by.GETDATECUSTOM () RETURNS DATETIME BEGIN return CONVERT(DATETIME,'{0}', 120); END";
+            query = String.Format(query, Date.getDate());
+            Sql.update(query);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Login());
